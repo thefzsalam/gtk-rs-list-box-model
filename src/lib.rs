b363465a3ac218547fbea03bed88e5833a07cb7e
Farzeen <12694111+thefzsalam@users.noreply.github.com>
2018-05-31 21:18:13 +0530

    Moved src/gobject_fzn/*.rs to src/*.rs Removed src/gobject_fzn/mod.rs to src/lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+use public_interface;
+use gobject_ptr_wrapper;
+use container_gobject;
+use list_box_model_gobject;
+use test_helpers;
+
+
+pub use self::public_interface::*;
+pub use self::gobject_ptr_wrapper::GObjectPtrWrapper;
+
