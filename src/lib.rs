30d8adbe51d249047b714e70d405ff6e090adf80
Farzeen <12694111+thefzsalam@users.noreply.github.com>
2018-05-31 21:36:45 +0530

    I should really revise the Rust book chapter explaining `use` and `mod` :/

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-use public_interface;
-use gobject_ptr_wrapper;
-use container_gobject;
-use list_box_model_gobject;
-use test_helpers;
+mod public_interface;
+mod gobject_ptr_wrapper;
+mod container_gobject;
+mod list_box_model_gobject;
+mod test_helpers;
 
 
-pub use self::public_interface::*;
-pub use self::gobject_ptr_wrapper::GObjectPtrWrapper;
-
+pub use public_interface::*;
+pub use gobject_ptr_wrapper::GObjectPtrWrapper;
